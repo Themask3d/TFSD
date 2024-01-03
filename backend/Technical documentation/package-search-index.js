01936018dd4393d72deff83b1a8a467f3b6a9833
The mask3d <145126152+Themask3d@users.noreply.github.com>
2024-01-03 18:41:40 +0100

    Delete backend directory

--- backend/Technical documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.bezkoder.spring.jpa.h2"},{"l":"com.bezkoder.spring.jpa.h2.controller"},{"l":"com.bezkoder.spring.jpa.h2.model"},{"l":"com.bezkoder.spring.jpa.h2.repository"}];updateSearchResults();
